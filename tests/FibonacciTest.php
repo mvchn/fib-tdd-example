07fec8f268f873df7696559f7c9cffdeae74b989
mvchn <movchan@gmail.com>
2022-05-01 18:19:21 +0300

    step 7

--- tests/FibonacciTest.php
+++ tests/FibonacciTest.php
@@ -9,9 +9,9 @@ class FibonacciTest extends TestCase
     function fibonacci(int $n)
     {
         if ($n === 0) {  return 0; }
-        if ($n <= 2) { return 1; }
+        if ($n === 1) { return 1; }
 
-        return 1 + 1;
+        return $this->fibonacci($n - 1) + $this->fibonacci($n - 2);
     }
 
     /**
@@ -28,5 +28,9 @@ class FibonacciTest extends TestCase
         yield 'one' => [1, 1];
         yield 'two' => [1, 2];
         yield 'three' => [2, 3];
+        yield 'four' => [3, 4];
+        yield 'five' => [5, 5];
+        yield 'six' => [8, 6];
+        yield 'seven' => [13, 7];
     }
 }
